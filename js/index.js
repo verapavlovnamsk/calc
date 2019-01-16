d4c0c0720c9eed9eff4d134764455ab565dc19f8
verapavlovnamsk <45511296+verapavlovnamsk@users.noreply.github.com>
2019-01-16 21:31:49 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,105 +1,86 @@
-class CalculatorModel {
-  constructor(input){
-    this._decimalAdded = false;
-    this._operators = ['+', '-', 'x', '÷'];
-    this._input = input;
-  }
 
-  get decimalAdded() {
-    console.log(this);
-    return this._decimalAdded;
-  }
-  
-  set decimalAdded(value){
-    if ( !(typeof value == 'boolean' || value instanceof boolean) ){
-      throw new TypeError('decimalAdded must be a boolean type!', "someFile.js", 14);
-    }
-    this._decimalAdded = value; 
-  }
-  
-  get input() {
-    return this._input;
-  }
-  
-  set input(value) {
-    if ( !(typeof value == 'string' || value instanceof String) ){
-      throw new TypeError('input must be a String type!', "someFile.js", 25);
-    }
-    this._input = value;
-  }
-  
-  get lastChar() {
-    return this._input.slice(-1);
-  }
-  
-  get operators() {
-    return this._operators;
-  }
+// Get all the keys from document
+var keys = document.querySelectorAll('#calculator span');
+var operators = ['+', '-', 'x', ':'];
+var decimalAdded = false;
+
+// Add onclick event to all the keys and perform operations
+for(var i = 0; i < keys.length; i++) {
+	keys[i].onclick = function(e) {
+		// Get the input and button values
+		var input = document.querySelector('.screen');
+		var inputVal = input.innerHTML;
+		var btnVal = this.innerHTML;
+		
+		// Now, just append the key values (btnValue) to the input string and finally use javascript's eval function to get the result
+		// If clear key is pressed, erase everything
+		if(btnVal == 'C') {
+			input.innerHTML = '';
+			decimalAdded = false;
+		}
+		
+		// If eval key is pressed, calculate and display the result
+		else if(btnVal == '=') {
+			var equation = inputVal;
+			var lastChar = equation[equation.length - 1];
+			
+			// Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
+			equation = equation.replace(/x/g, '*').replace(/:/g, '/');
+			
+			// Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
+			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
+				equation = equation.replace(/.$/, '');
+			
+			if(equation)
+				input.innerHTML = eval(equation);
+				
+			decimalAdded = false;
+		}
+		
+		// Basic functionality of the calculator is complete. But there are some problems like 
+		// 1. No two operators should be added consecutively.
+		// 2. The equation shouldn't start from an operator except minus
+		// 3. not more than 1 decimal should be there in a number
+		
+		// We'll fix these issues using some simple checks
+		
+		// indexOf works only in IE9+
+		else if(operators.indexOf(btnVal) > -1) {
+			// Operator is clicked
+			// Get the last character from the equation
+			var lastChar = inputVal[inputVal.length - 1];
+			
+			// Only add operator if input is not empty and there is no operator at the last
+			if(inputVal != '' && operators.indexOf(lastChar) == -1) 
+				input.innerHTML += btnVal;
+			
+			// Allow minus if the string is empty
+			else if(inputVal == '' && btnVal == '-') 
+				input.innerHTML += btnVal;
+			
+			// Replace the last operator (if exists) with the newly pressed operator
+			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+				// Here, '.' matches any character while $ denotes the end of string, so anything (will be an operator in this case) at the end of string will get replaced by new operator
+				input.innerHTML = inputVal.replace(/.$/, btnVal);
+			}
+			
+			decimalAdded =false;
+		}
+		
+		// Now only the decimal problem is left. We can solve it easily using a flag 'decimalAdded' which we'll set once the decimal is added and prevent more decimals to be added once it's set. It will be reset when an operator, eval or clear key is pressed.
+		else if(btnVal == '.') {
+			if(!decimalAdded) {
+				input.innerHTML += btnVal;
+				decimalAdded = true;
+			}
+		}
+		
+		// if any other key is pressed, just append it
+		else {
+			input.innerHTML += btnVal;
+		}
+		
+		// prevent page jumps
+		e.preventDefault();
+	} 
 }
-
-class CalculatorPresenter {
-
-  constructor(inputScreen){
-    this._inputScreen = inputScreen;
-    this._model = new CalculatorModel(this._inputScreen.innerHTML);
-  }
-  
-  get inputScreen(){
-    return this._inputScreen;
-  }
-    
-  get model(){
-    return this._model;
-  }
-  
-  addOperator(event) {
-    const operator = event.target.innerHTML;
-    if(this.model.input !== '' && this.model.operators.indexOf(this.model.lastChar) === -1){
-      this.inputScreen.innerHTML += operator;
-      this.model.input = this.inputScreen.innerHTML;
-    } else if(this.model.input === '' && operator === '-') {
-      this.inputScreen.innerHTML += operator;
-      this.model.input = this.inputScreen.innerHTML;
-    }
-    
-    if(this.model.operators.indexOf(this.model.lastChar) > -1 && this.model.input.length > 1) {
-      this.inputScreen.innerHTML = this.model.input.replace(/.$/, operator);
-      this.model.input = this.inputScreen.innerHTML;
-    }
-    this.model.decimalAdded = false;
-  }
-  
-  addNumeric(event) {
-    const number = event.target.innerHTML;
-    this.inputScreen.innerHTML += number;
-    this.model.input = this.inputScreen.innerHTML;
-  }
-  
-  addDecimal() {
-    if( !this.model.decimalAdded ){
-      this.inputScreen.innerHTML += '.';
-      this.model.input = this.inputScreen.innerHTML;
-      this.model.decimalAdded = true;
-    }
-  }
-  
-  evaulateExpression() {
-    this.model.input = this.model.input.replace(/x/g, '*').replace(/÷/g, '/');
-    if(this.model.operators.indexOf(this.model.lastChar) > -1 || this.model.lastChar === '.'){
-          this.model.input = this.model.input.replace(/.$/, '');
-    }
-    if(this.model.input) {
-      this.inputScreen.innerHTML = eval(this.model.input);
-      this.model.input = this.inputScreen.innerHTML;
-    }
-    this.model.decimalAdded = false;
-  }
-  
-  clearScreen() {
-    this.inputScreen.innerHTML = "";
-    this.model.input = this.inputScreen.innerHTML;
-    this.model.decimalAdded = false;
-  }
-}
-
-const presenter = new CalculatorPresenter(document.querySelector('#screen'));
